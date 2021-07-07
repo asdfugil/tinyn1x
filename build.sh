440a9697d6f3544d9e9f173c0cea394dca0585ce
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-07-07 21:21:19 +0800

    make this way better

--- build.sh
+++ build.sh
@@ -4,36 +4,54 @@ SYSLINUX="https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/syslinux
 KERN="./linux/arch/x86/boot/bzImage"
 RD="./initramfs.cpio.zst"
 VERSION=1.0
-export CFLAGS="-m32 -march=i486"
-export CXXFLAGS="-m32 -march=i486"
+export CFLAGS="-m32 -march=i486 -Os"
+export CXXFLAGS="-m32 -march=i486 -Os"
+export LDFLAGS="-m32"
 
 rm -rf rootfs syslinux-*/ CD_Root
+if [ "$1" = "clean" ];then
+cd linux && make distclean && cd ..
+cd busybox && make distclean && cd ..
+else
 # kernel
 if [ ! -f "./linux/.build_complete" ]; then
-cp .config linux
+cp assets/.config linux
 cd linux
+make -j$(nproc) && touch .build_complete
+cd ..
+fi
+# busybox
+if [ ! -f "./busybox/.build_complete" ]; then
+cp assets/busybox_config busybox/.config
+cd busybox
 make -j$(nproc) && touch .build_complete
 cd ..
 fi
 # initramfs
  # base structure
 mkdir -p rootfs/usr/{bin,sbin}
-mkdir -p rootfs/{dev,sys,proc,tmp,var,lib}
+mkdir -p rootfs/{dev,sys,proc,tmp,var,lib,etc}
+mkdir rootfs/dev/{shm,pts}
+chmod 777 rootfs/dev/shm
+chmod 755 rootfs/dev/pts
 ln -s /tmp rootfs/var/tmp
 ln -s /usr/bin rootfs/bin
 ln -s /usr/sbin rootfs/sbin
  # copy resources
-cp busybox rootfs/usr/bin
+cp busybox/busybox rootfs/usr/bin
 #wget -O rootfs/usr/bin/checkra1n $CHECKRA1N_I486_URL
 cp checkra1n rootfs/usr/bin/checkra1n
-cp init rootfs
-cp ld-musl-i386.so.1 rootfs/lib
+cp assets/{fs,init}tab rootfs/etc
+cp assets/init rootfs
+cp scripts/* rootfs/usr/bin
 chmod +x rootfs/usr/bin/checkra1n
  # device nodes
 cp -dpR /dev/tty[0-6] rootfs/dev
 cp -dpR /dev/{tty,console,null,zero,full} rootfs/dev
  # pack
 cd rootfs
+# upx -9 usr/bin/checkra1n
+# upx --ultra-brute -9 usr/bin/busybox
 find . | fakeroot cpio -oH newc | zstd -c19 > ../initramfs.cpio.zst
 cd ..
  # bootloader
@@ -56,3 +74,4 @@ cp isolinux.cfg CD_Root/isolinux
 # make .iso
 mkisofs -o boot.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table CD_Root
 isohybrid boot.iso
+fi
